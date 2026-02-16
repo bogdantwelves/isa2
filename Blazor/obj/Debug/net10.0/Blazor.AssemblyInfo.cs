59245eceae2f69c43688c38faceb814b3be8adc1
bogdantwelve <bogdantwelve@gmail.com>
2026-02-17 00:13:15 +0200

    adding todo

--- Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
+++ Blazor/obj/Debug/net10.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec5f031d858c77446942f39e072036ef967a4c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
