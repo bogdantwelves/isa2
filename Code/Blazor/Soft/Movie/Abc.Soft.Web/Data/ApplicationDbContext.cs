45efdb171eb840a329bae0e2a37132ced81c3570
bogdantwelve <bogdantwelve@gmail.com>
2026-02-21 17:22:05 +0200

    Finished 3rd lesson

---
+++ Code/Blazor/Soft/Movie/Abc.Soft.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abc.Soft.Web.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+        public DbSet<Model.Movie> Movie { get; set; } = default!;
+}
